7a788ef151f839a609240b3bb3855d159009729a
Mohammed-Ahmed010 <mohammedahme1212@gmail.com>
2024-02-25 21:18:31 +0530

    ok

---
+++ Phase2/balancingParenthesis.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+bool solve(string s){
+        stack<char>st;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('||s[i]== '{'||s[i]=='[')
+                st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+                char t=st.top();
+                if((t=='(' && s[i]==')')||(t=='{' && s[i]=='}')||(t=='[' &&s[i]==']')){
+                    st.pop();
+                }else{
+                    return false;
+                }
+                
+            }
+        }
+        if(st.empty()) return true;
+        return false;
+        
+    }
+
+int main(){
+    string s="{[()]}";
+    cout<<solve(s)<<"\n";
+}
+
